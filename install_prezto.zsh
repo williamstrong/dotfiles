ffe718f3afe931a4adc7739b7bed8eef6fbf3d84
William Strong <william.strong@me.com>
2018-06-29 23:04:39 -0700

    fix directory names

--- install_prezto.zsh
+++ install_prezto.zsh
@@ -4,7 +4,7 @@ for rcfile in "${ZDOTDIR:-$HOME}"/dotfiles/prezto/runcoms/^README.md(.N); do
   [[ $(basename $rcfile) == "zshrc" || $(basename $rcfile) == "zpreztorc" ]] || (echo -n "  linking $rcfile  " && ln -s "$rcfile" "${ZDOTDIR:-$HOME}/.${rcfile:t}")
 done
 
-echo -n "  linking dotfiles/.zshrc  " && ln -s ~/dotfiles/.zshrc ~/.zshrc && echo ""
-echo -n "  linking dotfiles/.zpreztorc  " && ln -s ~/dotfiles/.zpreztorc ~/.zpreztorc && echo ""
+echo -n "  linking dotfiles/zshrc  " && ln -s ~/dotfiles/zshrc ~/.zshrc && echo ""
+echo -n "  linking dotfiles/zpreztorc  " && ln -s ~/dotfiles/zpreztorc ~/.zpreztorc && echo ""
 
 exit 0
